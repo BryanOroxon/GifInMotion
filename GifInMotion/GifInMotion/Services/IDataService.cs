09774c17bfd0e0d7c3f2fdbfc99dae3618c8b5c6
Luis Bryan Oroxón Ramíre <lanzer2002@hotmail.com>
2019-12-14 15:01:50 -0600

    Initial Commit

---
+++ GifInMotion/GifInMotion/Services/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GifInMotion.Models;
+
+
+namespace GifInMotion.Services
+{
+    public interface IDataService
+    {
+        Task<IEnumerable<Movies>> GetCountriesAsync();
+    }
+}
---
+++ GifInMotion/GifInMotion/Services/WebDataService.cs
@@ -0,0 +1,24 @@
+﻿using GifInMotion.Models;
+using GifInMotion.Services;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(WebDataService))]
+namespace GifInMotion.Services
+{
+    public class WebDataService : IDataService
+    {
+        HttpClient httpClient;
+
+        HttpClient Client => httpClient ?? (httpClient = new HttpClient());
+
+        public async Task<IEnumerable<Movies>> GetCountriesAsync()
+        {
+            var json = await Client.GetStringAsync("https://raw.githubusercontent.com/BryanOroxon/DemoCollectionView/master/DemoCollectionView/DemoCollectionView/Data/countrydata.json");
+            var all = Movies.FromJson(json);
+            return all;
+        }
+    }
+}
